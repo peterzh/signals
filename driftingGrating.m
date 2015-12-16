b0dc5f6d09c9787af5d36767046ded96687508da
Chris Burgess <mail@chrisburgess.me.uk>
2015-12-16 13:24:29 +0000

    MEX project fixes

--- driftingGrating.m
+++ driftingGrating.m
@@ -5,7 +5,7 @@ function driftingGrating(t, evts, pars, stimuli, inp, out, audio)
 grating = vis.grating(t);    % we want a gabor grating patch
 grating.phase = 2*pi*t*3; % with it's phase cycling at 3Hz
 
-stimOff = evts.newTrial.delay(0.5); % stimOff occurs 0.5s after new trial starts
+stimOff = evts.newTrial.delay(pars.stimDuration); % stimOff occurs 0.5s after new trial starts
 evts.endTrial = stimOff.delay(1);  % next trial should start 1s after stimOff
 grating.show = evts.newTrial.to(stimOff);  % stimulus visible between trial onset & stimOff
 
