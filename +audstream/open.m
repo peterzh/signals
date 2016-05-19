9a87253f52f46f023776bbb48a79ecb44944cf24
Chris Burgess <mail@chrisburgess.me.uk>
2016-05-19 18:47:33 +0100

    Improved audio streaming. Can now merge multiple streams onto same device and configurable latency

---
+++ +audstream/open.m
@@ -0,0 +1,6 @@
+function id = open(sampleRate, nChannels, devIdx, suggestedLatency)
+%audstream.open Opens a new audio stream
+%   Detailed explanation goes here
+%
+%   id = audstream.open(sampleRate, [nChannels], [devIdx], [suggestedLatency])
+end
