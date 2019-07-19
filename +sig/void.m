947b5ed9f3ec4a0c3cf85c036ec02b3d5e962753
jaib1 <j.bhagat@ucl.ac.uk>
2019-07-19 10:35:33 +0100

    added 'setEpochTrigger' to 'VoidSignal'

--- +sig/void.m
+++ +sig/void.m
@@ -1,6 +1,6 @@
 function v = void(cache)
 %VOID A signal that never has any values
-%   TODO: make is so it really can't be updated!
+%   TODO: make it so it really can't be updated!
 if nargin < 1; cache = false; end
 v = sig.VoidSignal.instance(cache);
 end
